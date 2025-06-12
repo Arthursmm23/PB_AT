222129aa0f2cccedacd620ecfece59e6a5c287db
Arthur Moreira <162569297+Arthursmm23@users.noreply.github.com>
2025-06-12 17:21:44 -0300

    Delete Sistema - Copia-20250605T202609Z-1-001-20250606T004109Z-1-001/Sistema - Copia-20250605T202609Z-1-001 directory

--- Sistema - Copia-20250605T202609Z-1-001-20250606T004109Z-1-001/Sistema - Copia-20250605T202609Z-1-001/Sistema - Copia/src/main/java/org/example/repository/AlertaRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.repository;
-
-import org.example.model.Alerta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlertaRepository extends JpaRepository<Alerta, String> {
-}
