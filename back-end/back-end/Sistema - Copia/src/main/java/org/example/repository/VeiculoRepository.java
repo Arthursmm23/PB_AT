5d4adfce6c4ef31d4dcb3f6860563ada26da92f8
Arthur Moreira <162569297+Arthursmm23@users.noreply.github.com>
2025-06-16 10:31:01 -0300

    Delete back-end/back-end directory

--- back-end/back-end/Sistema - Copia/src/main/java/org/example/repository/VeiculoRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.repository;
-
-import org.example.model.Veiculo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface VeiculoRepository extends JpaRepository<Veiculo, String> {
-    List<Veiculo> findByTipo(String tipo);
-}
